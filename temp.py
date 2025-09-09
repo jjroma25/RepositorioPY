3f48ba748679a18510b4328121a758df148d0b5f
jjroma25 <145985519+jjroma25@users.noreply.github.com>
2025-09-09 10:58:20 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -17,3 +17,9 @@ print ('resta =', resta)
 
 multiplicacion = numero_uno * numero_dos
 print ('multiplicacion =', multiplicacion)
+
+division = numero_uno / numero_dos
+print ('division =', division)
+
+exponente = numero_uno ** numero_dos
+print ('exponente =', exponente)
